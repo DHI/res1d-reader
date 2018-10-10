6443510deaa01db9afaf4e83efde626a949bf12f
rha <rha@dhigroup.com>
2018-10-10 13:42:41 +0200

    Fixed namespace problems

--- DHI.Res1DReader.Test/Properties/AssemblyInfo.cs
+++ DHI.Res1DReader.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Res1DReader.Test")]
+[assembly: AssemblyTitle("DHI.Res1DReader.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Res1DReader.Test")]
+[assembly: AssemblyProduct("DHI.Res1DReader.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d3f6bc88-1bdb-4aa5-8a21-46f7b6c78570")]
+[assembly: Guid("4b59e790-e2b8-4f47-b1fb-504c773ad9e3")]
 
 // Version information for an assembly consists of the following four values:
 //
